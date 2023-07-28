a9be87d8499adce1bb90105e866ff95f84080476
smyang <lonesome661@naver.com>
2023-07-28 14:57:17 +0900

    prefix를 사용하도록 수정

--- PostgresSchemaExtractor/extract.sql
+++ PostgresSchemaExtractor/extract.sql
@@ -2,4 +2,4 @@ SELECT tablename
 FROM pg_catalog.pg_tables
 WHERE schemaname != 'pg_catalog'
     AND schemaname != 'information_schema'
-    AND tablename LIKE 'revision%'
+    AND tablename LIKE '{table_prefix}%'
